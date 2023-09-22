4694ebd8eb5871a47be570265f7d7cb54a6bdc8e
Oscar Sangrador <oscar@vivla.com>
2023-09-22 14:45:40 +0200

    feat: add prettier ignore file and disabled some eslint rules. Deploy functions in firebase

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -14,6 +14,9 @@ module.exports = {
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
     "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "indent": "off",
+    "object-curly-spacing": "off",
+    "comma-dangle": "off"
   },
   overrides: [
     {
